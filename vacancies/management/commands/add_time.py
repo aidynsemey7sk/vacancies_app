f05e044d7a3b6dc9091fcd46768dd0a4a045ac69
aidyn <aidynsemey7sk@gmail.com>
2023-02-07 16:21:40 +0600

    add_time

---
+++ vacancies/management/commands/add_time.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+import time
+from vacancies.models import Time
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        start_time = time.time()
+        for i in range(0, 24, 1):
+            t = f"{str(i)}:00:00"
+            Time.objects.create(t)
+
+
+        self.stdout.write("--- %s seconds ---" % (time.time() - start_time))
