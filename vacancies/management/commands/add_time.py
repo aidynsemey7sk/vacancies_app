37251fb1a03e7f76f6525299a49850b8a9418606
aidyn <aidynsemey7sk@gmail.com>
2023-02-07 16:29:32 +0600

    add_time

--- vacancies/management/commands/add_time.py
+++ vacancies/management/commands/add_time.py
@@ -8,6 +8,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         start_time = time.time()
         for i in range(0, 24, 1):
+            if len(str(i)) < 2:
+                i = "0" + str(i)
             t = f"{str(i)}:00:00"
             Time.objects.create(time=t)
 
